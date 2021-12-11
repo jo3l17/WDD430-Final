c438e7acbbfcf6199e3c4296b8a3732a3d1bca17
jo3l17 <joelvaldezangeles@gmail.com>
2021-12-11 12:07:44 -0500

    🔑 password validation added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,6 +8,11 @@ const userController = {
   // signup
   signup: async (req, res) => {
     const { password, email } = req.body;
+    if(password.length < 8) {
+      return res.status(400).json({
+        message: "Password must be at least 8 characters long"
+      });
+    }
     const cryptedPassword = bcrypt.hashSync(password, salt);
     const user = new User({ password: cryptedPassword, email });
     try {
